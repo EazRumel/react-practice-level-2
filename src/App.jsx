f59ace32f7a4812b287548328c60f321f4a62f41
Hemel <reaz.hemel@gmail.com>
2024-10-30 13:29:13 +0600

    first initialization of the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1 className='text-7xl'>My practice level-2</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
